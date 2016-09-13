88e98a14483f8026a77b4f34d4d984a1060ccaba
Michael Terpak <michaelterpak@gmail.com>
2016-09-13 16:06:44 -0400

    Major version upgrade for new scheduler

--- index.js
+++ index.js
@@ -1,179 +1,188 @@
 'use strict'
 
-const fs = require('fs')
+const Promise = require('bluebird')
 const superagent = require('superagent')
-let config = {}
 
-function Event({
-    slug,
-    key, 
-    endpoint,
-    run_at,
-    recurring,
-    payload,
-    fail_count
-} = {}) {
-    this.slug = slug
-    this.key = key
-    this.endpoint = endpoint
-    this.recurring = recurring
-    this.run_at = run_at ? new Date(run_at) : undefined,
-    this.fail_count = fail_count
-    this.payload = payload
+Promise.promisifyAll(superagent)
+
+// Errors relating to event-related errors
+class EventError extends Error {
+    constructor() {
+        super(...arguments)
+        this.name = 'EventError'
+    }
 }
-
-module.exports.add = function(slug, key, params, _trx) {
-    if (!config.endpoint) throw new Error('Missing event server endpoint in configuration')
-    return new Promise((resolve, reject) => {
-        if (!slug) return reject({
-            code: 400,
-            msg: 'Missing required parameter: slug'
-        })
-        
-        if (_trx) {
-            _trx.revert(() => {
-                this
-                    .remove(slug, key)
-                    .then()
-                    .catch(console.error)
-            })
-        }
-        
-        superagent
-            .post([config.endpoint, slug, key].join('/'))
-            .send(params)
-            .end((err, res) => {
-                if (err) reject(err.response.body)
-                else if (res.body.error) reject(res.body.error)
-                else {
-                    if (_trx) {
-                        
-                    }
-                    resolve(new Event(res.body.result))
-                }
-            })
-    })
-}
-
-module.exports.conf = {
-    loadFromPath: function(path) {
-        let data = fs.readFileSync()
-        try {
-            data = JSON.parse(data)
-        } catch(e) {
-            console.error(`Error parsing config file at ${path}`)
-            throw e
-        }
-        module.exports.update(data)
-    },
-    update: function(updates) {
-        for (let i in updates) {
-            if (updates.hasOwnProperty(i)) {
-                config[i] = updates[i]
-            }
-        }
+// Errors relating to the scheduling server
+class SchedulerError extends Error {
+    constructor() {
+        super(...arguments)
+        this.name = 'SchedulerError'
     }
 }
 
-module.exports.get = function(slug, key) {
-    if (!config.endpoint) throw new Error('Missing event server endpoint in configuration')
-    return new Promise((resolve, reject) => {
-        if (!slug) return reject({
-            code: 400,
-            msg: 'Missing required parameter: slug'
-        })
-        
-        superagent
+class Event {
+
+    /**
+     * new Event
+     * 
+     * @param options {Object}
+     * @param options.slug {String}
+     * @param options.key {String} optional
+     * @param options.run_at {Date|Integer}
+     * @param options.recurring {false|Object} see "recurring"
+     * @param options.failed {Boolean} optional, default false
+     * @param options.failed_code {null|Integer} optional, default null
+     * @param options.failed_reason {null|String|Object} optional, default null
+     * @param options.request {Object}
+     * @param options.request.host {String}
+     * @param options.request.protocol {String} optional, default 'http:'
+     * @param options.request.port {Integer} optional, default 80
+     * @param options.request.headers {Object}
+     * @param options.request.method {String} optional, default 'GET'
+     * @param options.request.path {String} optional, default '/'
+     * @param options.request.data {String|Object}
+     */
+    constructor({
+        slug,
+        key = '',
+        request: {
+            host,
+            protocol = 'http:',
+            port = 80,
+            headers = {},
+            method = 'GET',
+            path = '/',
+            data
+        } = {},
+        run_at,
+        recurring = false,
+        failed = false,
+        failed_code = null,
+        failed_reason = null
+    } = {}) {
+        this.slug = slug
+        this.key = key
+        this.request = {
+            host,
+            protocol: protocol.substr(-1) === ':' ? protocol : protocol + ':',
+            port,
+            headers,
+            method,
+            path: path[0] === '/' ? path : '/' + path,
+            data
+        },
+        this.run_at = new Date(run_at)
+        this.recurring = recurring
+        this.failed = failed
+        this.failed_code = failed_code
+        this.failed_reason = failed_reason
+    }
+}
+
+class Scheduler {
+
+    /**
+     * @param options {Object}
+     * @param options.host {String} optional, default 'localhost'
+     * @param options.port {Integer} optional, default 5665
+     */
+    constructor({ host = 'localhost', port = 5665 } = {}) {
+        this.endpoint = `http://${host}:${port}`
+    }
+
+    /**
+     * Create a new event
+     * 
+     * @param slug {String}
+     * @param key {String} optional
+     * @param params {Object} see "new Event"
+     * 
+     * @returns {Promise}
+     */
+    add(slug, key = '', params = {}) {
+        if (!slug) {
+            let err = new EventError('Missing required parameter: slug')
+            err.status = 400
+            return Promise.reject(err)
+        }
+
+        return superagent
+            .post([this.endpoint, slug, key].join('/'))
+            .send(params)
+            .endAsync()
+            .then(res => new Event(res.body.result))
+            .catch(err => Promise.reject(SchedulerError(err)))
+    }
+
+    /**
+     * Get the event with the following slug and key
+     * 
+     * @param slug {String}
+     * @param key {String} optional
+     * 
+     * @returns {Promise}
+     */
+    get(slug, key = '') {
+        if (!slug) {
+            let err = new EventError('Missing required parameter: slug')
+            err.status = 400
+            return Promise.reject(err)
+        }
+
+        return superagent
             .get([config.endpoint, slug, key].join('/'))
-            .end((err, res) => {
-                if (err) reject(err.response.body.error)
-                else if (res.body.error) reject(res.body.error)
-                else resolve(res.body.result ? new Event(res.body.result) : null)
-            })
-    })
-}
+            .endAsync()
+            .then(res => new Event(res.body.result))
+            .catch(err => Promise.reject(SchedulerError(err)))
+    }
 
-module.exports.remove = function(slug, key, _trx) {
-    if (!config.endpoint) throw new Error('Missing event server endpoint in configuration')
-    return new Promise((resolve, reject) => {
-        if (!slug) return reject({
-            code: 400,
-            msg: 'Missing required parameter: slug'
-        })
-        
-        if (!_trx) {
-            go()
-        } else {
-            this
-                .get(slug, key)
-                .then(sch => {
-                    if (!sch) return reject({
-                        code: 404,
-                        msg: 'Schedule not found'
-                    })
-                    
-                    _trx.revert(() => {
-                        this
-                            .add(slug, key, sch)
-                            .then()
-                            .catch(console.error)
-                    })
-                    
-                    go()
-                })
-                .catch(reject)
+    /**
+     * Remove the event with the following slug and key
+     * 
+     * @param slug {String}
+     * @param key {String} optional
+     * 
+     * @returns {Promise}
+     */
+    remove(slug, key = '') {
+        if (!slug) {
+            let err = new EventError('Missing required parameter: slug')
+            err.status = 400
+            return Promise.reject(err)
         }
-        
-        function go() {
-            superagent
-                .del([config.endpoint, slug, key].join('/'))
-                .end((err, res) => {
-                    if (err) reject(err.response.body.error)
-                    else if (res.body.error) reject(res.body.error)
-                    else resolve(res.body.result.deleted)
-                })
-        }
-    })
-}
 
-module.exports.update = function(slug, key, updates, _trx) {
-    if (!config.endpoint) throw new Error('Missing event server endpoint in configuration')
-    return new Promise((resolve, reject) => {
-        if (!slug) return reject({
-            code: 400,
-            msg: 'Missing required parameter: slug'
-        })
-        
-        if (!_trx) {
-            go()
-        } else {
-            this
-                .get(slug, key)
-                .then(sch => {
-                    if (!sch) return reject({
-                        code: 404,
-                        msg: 'Schedule not found'
-                    })
-                    
-                    _trx.revert(() => {
-                        this
-                            .update(slug, key, sch)
-                            .then()
-                            .catch(console.error)
-                    })
-                    
-                    go()
-                })
-                .catch(reject)
+        return superagent
+            .del([config.endpoint, slug, key].join('/'))
+            .endAsync()
+            .then(res => res.body.result.deleted)
+            .catch(err => Promise.reject(SchedulerError(err)))
+    }
+    
+    /**
+     * Update the event matching the slug+key with the following updates
+     * 
+     * @param slug {String}
+     * @param key {String} optional
+     * @param updates {Object} see "new Event"
+     * 
+     * @returns {Promise}
+     */
+    update(slug, key = '', updates = {}) {
+        if (!slug) {
+            let err = new EventError('Missing required parameter: slug')
+            err.status = 400
+            return Promise.reject(err)
         }
-        
-        superagent
+
+        return superagent
             .put([config.endpoint, slug, key].join('/'))
             .send(updates)
-            .end((err, res) => {
-                if (err) reject(err.response.body.error)
-                else if (res.body.error) reject(res.body.error)
-                else resolve(new Event(res.body.result))
-            })
-    })
+            .endAsync()
+            .then(res => new Event(res.body.result))
+            .catch(err => SchedulerError(err))
+    }
 }
+
+module.exports = options => new Scheduler(options)
+module.exports.EventError = EventError
+module.exports.SchedulerError = SchedulerError
--- test.js
+++ test.js
@@ -6,29 +6,4 @@ events.conf.update({
     endpoint: ''
 })
 
-events
-    .add('assignment', 'bunman', {
-        run_at: new Date(Date.now() + 3600000)
-    })
-    .then(console.log)
-    .catch(console.error)
-events
-    .get('assignment', 'bunman')
-    .then(console.log)
-    .catch(console.error)
-events
-    .update('assignment', 'bunman', { endpoint: String(Math.floor(Math.random() * 100000)) })
-    .then(console.log)
-    .catch(console.error)
-events
-    .get('assignment', 'bunman')
-    .then(console.log)
-    .catch(console.error)
-events
-    .remove('assignment', 'bunman')
-    .then(console.log)
-    .catch(console.error)
-events
-    .get('assignment', 'bunman')
-    .then(console.log)
-    .catch(console.error)
+// TODO tests
